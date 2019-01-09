3d33e2ee2d8cd1372098c1c337c1e84f0bed004a
fernaperg <fernaperg@gmail.com>
2019-01-09 18:17:20 +0100

    :pencil: Updated comments

--- 4kyu/TwiceLinear.py
+++ 4kyu/TwiceLinear.py
@@ -1,6 +1,6 @@
 """
 	Solution of the problem
-	Decode the morse code advanced
+	Twice linear
 	https://www.codewars.com/kata/5672682212c8ecf83e000050
 
 	@author Fernando Pérez Gutiérrez <fernaperg@gmail.com>
@@ -8,10 +8,10 @@
 from collections import deque
 def dbl_linear(n):
     h = 1
-    cnt = 0
+    i = 0
     q1, q2 = deque([]), deque([])
     while True:
-        if cnt >= n:
+        if i >= n:
             return h
         q1.append(2*h+1)
         q2.append(3*h+1)
@@ -20,4 +20,4 @@ def dbl_linear(n):
             h = q1.popleft()
         if h == q2[0]:
             h = q2.popleft()
-        cnt += 1
+        i += 1
