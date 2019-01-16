4ab92b35c188b0c87f5dc299ddadcb982f75a755
fernaperg <fernaperg@gmail.com>
2019-01-16 19:14:52 +0100

    :sparkles: The easiest kata ever. In bash

---
+++ 8kyu/Opposite Number.sh
@@ -0,0 +1,9 @@
+: '
+	Solution of the problem
+	Opposite Number
+	https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+	@author Fernando Pérez Gutiérrez <fernaperg@gmail.com>
+'
+#!/bin/bash
+echo "$1 * -1" | bc -l
